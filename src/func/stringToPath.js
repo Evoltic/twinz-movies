c01366b07bda90a24763c8df51783352e99e9ea1
Evoltic <32727622+Evoltic@users.noreply.github.com>
2018-05-10 20:07:14 +0300

    changed comment

--- src/func/stringToPath.js
+++ src/func/stringToPath.js
@@ -2,6 +2,6 @@ const stringToPath = (title, id) => (
   'id' + id + '-' + title.replace(/[^A-Za-z0-9^ ^-]/g, '').replace(/ /g, '-').toLowerCase()
 )
 
-// for example: 'Avengers-10: Infinity Movie' becomes '1234-avengers-10-infinity-war'
+// for example: 'Avengers-10: Infinity Movie' becomes 'id1234-avengers-10-infinity-war'
 
 export default stringToPath;
